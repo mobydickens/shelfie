cce3f3d1962303a43be99fde2280061c985f86d2
mobydickens <steadmegan@gmail.com>
2018-11-30 10:26:23 -0700

    Step 1 - setting up form layout and input boxes with state

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -2,9 +2,51 @@ import React, { Component } from 'react';
 
 class Form extends Component {
 
+  constructor() {
+    super();
+    this.state = {
+      image_url: '',
+      product_name: '',
+      price: ''
+    }
+  }
+
+  handleImageChange = (value) => {
+    this.setState({
+      image_url: value
+    })
+  }
+
+  handleProductChange = (value) => {
+    this.setState({
+      product_name: value
+    })
+  }
+
+  handlePriceChange = (value) => {
+    this.setState({
+      price: value
+    })
+  }
+  
+  cancel = () => {
+    this.setState({
+      image_url: '',
+      product_name: '',
+      price: ''
+    })
+  }
+
   render() {
     return(
-      <div>Form</div>
+      <div>
+        <div>Form</div>
+        <input type="text" placeholder="image url" onChange={ (e) => this.handleImageChange(e.target.value) } value={ this.state.image_url }/>
+        <input type="text" placeholder="product name" onChange={ (e) => this.handleProductChange(e.target.value) } value={ this.state.product_name }/>
+        <input type="text" placeholder="price" onChange={ (e) => this.handlePriceChange(e.target.value) } value={ this.state.price }/>
+        <button onClick={ () => this.cancel() }>Cancel</button>
+        <button>Add to Inventory</button>
+      </div>
     )
   }
 }
